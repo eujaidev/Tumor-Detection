165cdb45d3949867699b66de707a68307c15349e
eujaidev <eujaidev28@gmail.com>
2024-09-25 16:01:11 +0530

    Update

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from tensorflow.keras.models import load_model
+import tensorflow as tf
 import numpy as np
 from PIL import Image
 
 # Load the trained model
-model = load_model('cnn_tumor.keras')
+model = tf.keras.models.load_model('cnn_tumor.keras')
 
 # Define the prediction function
 def make_prediction(img, model):
